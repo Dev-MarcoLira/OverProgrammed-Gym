897e81be058c8ccf3862f826e3b46c37a2308e2f
Marco Antonio Lira Barros <malirab2302@gmail.com>
2025-04-07 22:53:04 -0300

    Add class Class

--- src/main/java/br/cefet/Class.java
+++ src/main/java/br/cefet/Class.java
@@ -1,26 +1,18 @@
 package br.cefet;
 
-import java.util.Random;
-
 public class Class {
  
     private String descricao;
-    private int id;
-    private int trainerId;
 
-    public Class(){
-        Random random = new Random();
-        int rand = random.nextInt(1000000) + 1;
-        this.id = rand;
+    public Class(
+        String descricao
+    ){
+        setDescricao(descricao);
     }
 
-    public void criarAula(String descricao, int trainerId){
+    public void createClass(String descricao, Trainer trainer){
         this.descricao = descricao;
-        if(trainerId != 0){
-            this.trainerId = trainerId;
-        }else{
-            System.out.println("Não foi possível criar a aula");
-        }
+        trainer.addClass(this);
     }
 
     public String getDescricao() {
@@ -29,22 +21,6 @@ public class Class {
 
     public void setDescricao(String descricao) {
         this.descricao = descricao;
-    }
-
-    public int getId() {
-        return this.id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getTrainerId() {
-        return this.trainerId;
-    }
-
-    public void setTrainerId(int trainerId) {
-        this.trainerId = trainerId;
     }
 
 }
